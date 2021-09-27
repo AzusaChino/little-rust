39a27606572e1d44d5eebb8d087380048586c6e8
AzusaChino <azusa146@gmail.com>
2021-09-27 11:34:46 +0800

    ft. adapt to new format

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # little-rust
 
 the path of conquering rust.
+
+## learn from microsoft [path](https://docs.microsoft.com/en-us/learn/paths/rust-first-steps/)
--- src/learning/practice/hello.rs
+++ src/learning/practice/hello.rs
@@ -21,10 +21,10 @@ fn shadow() {
     // The first binding is created with the name "number"
     let number = 5;
 
-    // A different binding shadows the name "number" 
-    let number = number + 5; 
+    // A different binding shadows the name "number"
+    let number = number + 5;
 
     // Again, another new binding is created
-    let number = number * 2; 
+    let number = number * 2;
     println!("The number is {}.", number);
 }
