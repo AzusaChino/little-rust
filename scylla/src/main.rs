2932186e30df2e99c02cc74fe42d53db3800f608
AzusaChino <azusa146@gmail.com>
2022-02-08 16:08:45 +0800

    ScyllaDB practice

---
+++ scylla/src/main.rs
@@ -0,0 +1,44 @@
+use anyhow::Result;
+use uuid::Uuid;
+
+use crate::duration::Duration;
+use crate::temperature_measurement::TemperatureMeasurement;
+
+mod db;
+mod duration;
+mod temperature_measurement;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("connecting to scylla db");
+    let uri = std::env::var("SCYLLA_URI").unwrap_or_else(|_| String::from("127.0.0.1:9042"));
+    let session = db::create_session(&uri).await?;
+    db::initialize(&session).await?;
+
+    println!("Adding measurements");
+    let measurement = TemperatureMeasurement {
+        device: Uuid::parse_str("72f6d49c-76ea-44b6-b1bb-9186704785db")?,
+        time: Duration::seconds(1000000000001),
+        temperature: 40,
+    };
+    db::add_measurement(&session, measurement).await?;
+
+    let measurement = TemperatureMeasurement {
+        device: Uuid::parse_str("72f6d49c-76ea-44b6-b1bb-9186704785db")?,
+        time: Duration::seconds(1000000000003),
+        temperature: 60,
+    };
+    db::add_measurement(&session, measurement).await?;
+
+    println!("Selecting measurements");
+    let measurements = db::select_measurements(
+        &session,
+        Uuid::parse_str("72f6d49c-76ea-44b6-b1bb-9186704785db")?,
+        Duration::seconds(1000000000000),
+        Duration::seconds(10000000000009),
+    )
+        .await?;
+    println!("     >> Measurements: {:?}", measurements);
+
+    OK(())
+}
