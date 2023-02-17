012717b40ba35aa33f5ac2961bdf2ad28e3c15cf
azusachino <azusa146@gmail.com>
2023-02-17 13:51:56 +0800

    target_os specification

--- src/bin/nixs.rs
+++ src/bin/nixs.rs
@@ -1,32 +1,65 @@
+pub trait Fork {
+    /**
+     * use specific api to fork a child process
+     */
+    fn try_fork();
+}
+
+#[derive(Debug, Default)]
+pub struct Handler;
+
 #[cfg(target_os = "windows")]
-fn main() {
-    println!("Oops, It's windows")
-}
+mod windows {
+    use super::{Fork, Handler};
 
-#[cfg(target_os = "linux")]
-fn main() {
-    try_fork();
-}
-
-#[cfg(target_os = "linux")]
-fn try_fork() {
-    use nix::sys::wait::waitpid;
-    use nix::unistd::{fork, write, ForkResult};
-
-    match unsafe { fork() } {
-        Ok(ForkResult::Parent { child, .. }) => {
-            println!(
-                "Continuing execution in parent process, new child has pid: {}",
-                child
-            );
-            waitpid(child, None).unwrap();
+    impl Fork for Handler {
+        fn try_fork() {
+            unimplemented!("wait for windows");
         }
-        Ok(ForkResult::Child) => {
-            // Unsafe to use `println!` (or `unwrap`) here. See Safety.
-            write(libc::STDOUT_FILENO, "I'm a new child process\n".as_bytes()).ok();
-            // 退出子线程代码块, 不会kill子线程
-            unsafe { libc::_exit(0) };
-        }
-        Err(_) => eprintln!("fork failed"),
     }
+}
+
+#[cfg(target_os = "linux")]
+mod linux {
+    use super::{Fork, Handler};
+
+    impl Fork for Handler {
+        fn try_fork() {
+            use nix::sys::wait::waitpid;
+            use nix::unistd::{fork, write, ForkResult};
+
+            match unsafe { fork() } {
+                Ok(ForkResult::Parent { child, .. }) => {
+                    println!(
+                        "Continuing execution in parent process, new child has pid: {}",
+                        child
+                    );
+                    waitpid(child, None).unwrap();
+                }
+                Ok(ForkResult::Child) => {
+                    // Unsafe to use `println!` (or `unwrap`) here. See Safety.
+                    write(libc::STDOUT_FILENO, "I'm a new child process\n".as_bytes()).ok();
+                    // 退出子线程代码块, 不会kill子线程
+                    unsafe { libc::_exit(0) };
+                }
+                Err(_) => eprintln!("fork failed"),
+            }
+        }
+    }
+}
+
+#[cfg(target_os = "macos")]
+mod macos {
+    use super::{Fork, Handler};
+
+    impl Fork for Handler {
+        fn try_fork() {
+            unimplemented!("wait for windows");
+        }
+    }
+}
+
+fn main() {
+    // use different try_fork by current os
+    Handler::try_fork();
 }
